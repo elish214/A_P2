337584bf9a051d415ee5b6cec078223e6443c851
Raz <raz.s121@gmail.com>
2017-04-03 12:58:50 +0300

    try1

---
+++ A_P2/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_P2
+{
+    class Class1
+    {
+        public Maze maze;
+    }
+}
