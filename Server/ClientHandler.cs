3724a1dffe81a49f46f87a9f15b469a3d86b8598
Elisheva Broyer <elish214@gmail.com>
2017-04-05 17:24:22 +0300

    mission B began

---
+++ Server/ClientHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Sockets;
+
+
+namespace Server
+{
+    public class ClientHandler : IClientHandler
+    {
+        public void HandleClient(TcpClient client)
+        {
+            new Task(() =>
+            {
+                using (NetworkStream stream = client.GetStream())
+                using (StreamReader reader = new StreamReader(stream))
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    string commandLine = reader.ReadLine();
+                    Console.WriteLine("Got command: {0}", commandLine);
+                    string result = ExecuteCommand(commandLine, client);//execute-command pattern by dict.
+                    writer.Write(result);
+                }
+                client.Close();
+            }).Start();
+            
+        }
+    }
+}
