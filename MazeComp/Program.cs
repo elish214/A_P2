954b5f28639220fa2bc82f07da8c9206ec4ee0ec
Raz <raz.s121@gmail.com>
2017-04-08 20:08:24 +0300

    Mission A - making MazeComp as console app

--- MazeComp/Program.cs
+++ MazeComp/Program.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MazeComp;
 using MazeLib;
 using MazeGeneratorLib;
 using SearchAlgorithmsLib;
 using SearchAlgorithmsLib.searchers;
 
-namespace A_P2
+namespace MazeComp
 {
     class Program
     {
